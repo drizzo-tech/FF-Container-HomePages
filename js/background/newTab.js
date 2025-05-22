8f9e5a823f95e4547dff4cf1d7d20cb11704568b
Mike Olden <molden@GJXV7LL6WH.corp.proofpoint.com>
2025-05-22 09:58:18 -0700

    add check for pop-up

--- js/background/newTab.js
+++ js/background/newTab.js
@@ -1,37 +1,52 @@
-
-
 const newTab = {
-  isNewTab(tab){
+  async isNewTab(tab) {
     // checking openerTabId this way prevents loading default url to links that are directed to new tabs
-    // such as "Open in New Tab". Otherwise, checking only the tab title and url is not enough as both 
+    // such as "Open in New Tab". Otherwise, checking only the tab title and url is not enough as both
     // will keep updating while the tab status changes from "loading" to "complete".
     // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/tabs/onCreated
     // We could wait for the tab to finish loading and check the title and the url but that's time wasted.
-    // There yet to exist a legitimate case where checking openerTabId does not work. 
+    // There yet to exist a legitimate case where checking openerTabId does not work.
     // known issue: this won't work in case of opening "about:newtab" link as "Open in New Tab"
     // but arguably that's not a real use case.
 
     // this used to only check the openerTabId and worked for most cases, but caused a conflict with the MAC ext. issue #1
-    // so checking the URL and title were added, which requires the tab permission. 
+    // so checking the URL and title were added, which requires the tab permission.
     // Checking the URL alone should be enough but keeping the openerTabId check should not cause issues.
-    let isNew = tab.openerTabId === undefined && (tab.url === "about:newtab" || tab.url === "about:blank") && tab.title === "New Tab";
+    //
+    // Added check for pop-up to not redirect back to homepage
+    if (tab.windowId) {
+      try {
+        const window = await browser.windows.get(tab.windowId);
+        if (window.type === "popup") {
+          return false;
+        }
+      } catch (e) {
+        console.log("Error checking window type: ", e);
+      }
+    }
+    let isNew =
+      tab.openerTabId === undefined &&
+      (tab.url === "about:newtab" || tab.url === "about:blank") &&
+      tab.title === "New Tab";
     return isNew;
   },
-  async onCreated(tab){
-    if (newTab.isNewTab(tab)){
-      try{
-        const defaultUrl = await containerDefaultPages.getDefaultPage(tab.cookieStoreId)
-        if(defaultUrl){
-          browser.tabs.update(tab.tabId, {url: defaultUrl});
+  async onCreated(tab) {
+    if (await newTab.isNewTab(tab)) {
+      try {
+        const defaultUrl = await containerDefaultPages.getDefaultPage(
+          tab.cookieStoreId,
+        );
+        if (defaultUrl) {
+          browser.tabs.update(tab.tabId, { url: defaultUrl });
         }
-      }catch(e){
-        console.log(e)
+      } catch (e) {
+        console.log(e);
       }
     }
   },
-  init(){
+  init() {
     browser.tabs.onCreated.addListener(this.onCreated);
   },
-}
+};
 
-newTab.init()
+newTab.init();
